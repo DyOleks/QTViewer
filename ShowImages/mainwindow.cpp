dc3fff23b5882b4e782559c2ef842d0d17fbdbc7
DyOleks <dyakov.oleksandr@hotmail.com>
2022-08-20 15:23:10 +0200

    RQ_2 and RQ_3 have been implemented

--- ShowImages/mainwindow.cpp
+++ ShowImages/mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent, Qt::Window),
@@ -11,4 +13,25 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    //assume the directory exists and contains some files (otherwise should be specified in reqs?)
+    //we want all jpg files, .tiff (not a .tif) and .bmp
+    QString dir = QFileDialog::getExistingDirectory(this, tr("Open Directory"),
+                                                 "/home",
+                                                 QFileDialog::ShowDirsOnly
+                                                 | QFileDialog::DontResolveSymlinks);
+
+    QDir directory(dir);
+    QStringList images = directory.entryList(QStringList()  << "*.jpg"
+                                                            << "*.tiff"
+                                                            << "*.bmp", QDir::Files);
+    foreach(QString filename, images) {
+        //put all file names into container
+        //(can be implemented in a separate thread if we have big abmount of data)
+        qDebug()<<filename;
+        listOfFileNames.push_back(filename);
+    }
 }
